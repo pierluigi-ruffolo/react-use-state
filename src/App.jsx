ac606996b1d06374e802b4f09e37855b4ef6c476
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-28 13:07:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { languages } from "./dati/languages";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Learn web development</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
