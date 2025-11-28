91c34ba22116099bd8734a4778f77b907c0ef5e4
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-28 15:05:47 +0100

    feat: prove per integrare logica

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,31 @@ import { languages } from "./dati/languages";
 import "./App.css";
 
 function App() {
+  const [selectlanguages, setSelectLanguages] = useState(null);
+
   return (
     <>
       <header>
         <h1>Learn web development</h1>
       </header>
+      <div className="container">
+        <div className="d-flex">
+          {languages.map((language) => {
+            return (
+              <button
+                onClick={() => setSelectLanguages(language)}
+                key={language.id}
+              >
+                {language.title}
+              </button>
+            );
+          })}
+        </div>
+        <div className="card">
+          <h2>{selectlanguages === null ? "ciao" : selectlanguages.title}</h2>
+          <p></p>
+        </div>
+      </div>
     </>
   );
 }
